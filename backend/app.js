1110b71783d962d7117355c1c4029ea4100bd5b1
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-08-19 00:58:29 -0500

    feat: :sparkles: Conexión con mongo
    implementación de la conexión con mongodb
    
    campuslands

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import data from './settings.js';
+import data from './settings.js'; // variables de entorno
+import db from './connection/conect.js'; // conección
 
 const app = express();
 app.listen(data.server_config, ()=>{
