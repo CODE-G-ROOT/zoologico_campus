ff4828dd96f2690e4988e511fa7248b2f11e53e3
Muertoparado <nicolas.casanova20@gmail.com>
2023-09-04 08:10:54 -0500

    feat: :bug: correccion bd

--- backend/controllers/empleados.js
+++ backend/controllers/empleados.js
@@ -4,7 +4,6 @@ import { ObjectId } from "mongodb";
 export async function getEmpleados(req, res) {
     try {
         let db = await con();
-        console.log("get function");
         let colleccion = db.collection("empleados");
         let results = await colleccion.find({}).sort({ fecha: -1 }).toArray();
         results.length > 0 ? res.send(results).status(200) : res.status(404).send({ status: 404, message: "No Encontrado" })
