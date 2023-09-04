f27c058d2c96e76c9e892eec21ad66a2a2a4f617
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-04 11:23:57 -0500

    feat: :sparkles: recordatorio en el env.js
    pequeña configuración de reocrdatorio

--- backend/helpers/gen/env.js
+++ backend/helpers/gen/env.js
@@ -1,8 +1,8 @@
 import fs from 'fs/promises';
 
 const contenido = `
-ATLAS_USER=""
-ATLAS_PASSWORD=""
+ATLAS_USER="JuanDev856"
+ATLAS_PASSWORD="juan856"
 ATLAS_DB="zoologico"
 JWT_PRIVATE_KEY="cc"
 MY_SERVER={"hostname":"127.1.1.1","port":5510}
@@ -14,6 +14,7 @@ const archivo = '.env';
     try {
         await fs.writeFile(archivo, contenido);
         console.log(`Se ha creado el archivo "${archivo}"`);
+        console.warn("No te olvides de agregar tus credenciales en el archivo .env\n");
     } catch (err) {
         console.error('Error al crear el archivo:', err);
     }
